d74d3b759421a0828414c1dad1f2de5673acd88b
wkyrrrr <xuwenjiang2006@aliyun.com>
2021-09-24 17:19:58 +0800

    Initial commit

---
+++ src/main/java/com/geektime/spring/hello/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.geektime.spring.hello.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author abner
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello, Spring. Terminal";
+    }
+}
