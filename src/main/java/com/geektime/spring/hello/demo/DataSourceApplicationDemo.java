07581e94bb5c7aa1b8d09b23c9ca04faa19e89b7
wkyrrrr <xuwenjiang2006@aliyun.com>
2021-09-28 10:33:26 +0800

    Spring学习第二课：数据源配置

---
+++ src/main/java/com/geektime/spring/hello/demo/DataSourceApplicationDemo.java
@@ -0,0 +1,38 @@
+package com.geektime.spring.hello.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * @author abner
+ */
+@SpringBootApplication
+@Slf4j
+public class DataSourceApplicationDemo implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSourceApplicationDemo.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        showConnection();
+    }
+
+    private void showConnection() throws SQLException {
+        log.info(dataSource.toString());
+        Connection connection = dataSource.getConnection();
+        log.info(connection.toString());
+        connection.close();
+    }
+}
--- src/main/java/com/geektime/spring/hello/demo/DemoApplication.java
+++ src/main/java/com/geektime/spring/hello/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.geektime.spring.hello.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
